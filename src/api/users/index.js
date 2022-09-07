2ca74dec2f7848a8f9730c5566663cf1f7d6f0e7
SidathDabare <sidath2007@gmail.com>
2022-09-07 14:55:43 +0200

    homework

---
+++ src/api/users/index.js
@@ -0,0 +1,107 @@
+/** @format */
+
+import express from "express"
+import createHttpError from "http-errors"
+import UserModel from "./model.js"
+import { basicAuthMiddleware } from "../../lib/auth/basic.js"
+
+const usersRouter = express.Router()
+
+usersRouter.post("/", async (req, res, next) => {
+  try {
+    const newUser = new UserModel(req.body)
+    const { _id } = await newUser.save()
+
+    res.status(201).send({ _id })
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/", basicAuthMiddleware, async (req, res, next) => {
+  try {
+    const users = await UserModel.find()
+    res.send(users)
+  } catch (error) {
+    next(error)
+  }
+})
+usersRouter.get("/me", basicAuthMiddleware, async (req, res, next) => {
+  try {
+    //const user = await UserModel.find()
+    res.send(req)
+  } catch (error) {
+    next(error)
+  }
+})
+usersRouter.put("/me", basicAuthMiddleware, async (req, res, next) => {
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      req.user._id,
+      req.body,
+      { new: true, runValidators: true }
+    )
+    if (updatedUser) {
+      res.send(updatedUser)
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.delete("/me", basicAuthMiddleware, async (req, res, next) => {
+  try {
+    await UserModel.findByIdAndDelete(req.user._id)
+    res.status(204).send()
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/:userId", async (req, res, next) => {
+  try {
+    const user = await UserModel.findById(req.params.userId)
+    if (user) {
+      res.send(user)
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.put("/:userId", async (req, res, next) => {
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      req.params.userId,
+      req.body,
+      { new: true, runValidators: true }
+    )
+
+    if (updatedUser) {
+      res.send(updatedUser)
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.delete("/:userId", async (req, res, next) => {
+  try {
+    const deletedUser = await UserModel.findByIdAndDelete(req.params.userId)
+    if (deletedUser) {
+      res.status(204).send()
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default usersRouter
